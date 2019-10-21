368d1d72c8fba5ed35e5d6396dc0d7be42ccf46e
berkelmas <berkelmas96@gmail.com>
2019-10-21 22:55:38 +0300

    custom color palette for angular material implemented

--- src/app/dashboard/pages/home/home.module.ts
+++ src/app/dashboard/pages/home/home.module.ts
@@ -3,10 +3,17 @@ import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './home-page/home-page.component';
 import { HomeRoutingModule } from './home-routing.module';
 
+// ANGULAR MATERIAL
+import { MatButtonModule } from '@angular/material/button';
+
+
 @NgModule({
   declarations: [HomePageComponent],
   imports: [
     CommonModule,
+
+    // ANGULAR MATERIAL
+    MatButtonModule,
 
     HomeRoutingModule
   ]
